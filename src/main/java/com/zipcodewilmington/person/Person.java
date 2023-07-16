40c98f6a8fffad6d9e7eb3fba815a10a9623fea0
olamide <oadams@fandm.edu>
2023-07-16 13:32:04 -0400

    complete

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,67 @@ public class Person {
     private String name;
     private int age;
 
+    private int weight;
+    private int height;
+    private String eyecolor;
+    private String race;
+    private String gender;
+
     public Person() {
+       name = "";
+       age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        // since both variables share the same name we use "this.age" to call the one under the private field
+        this.age = age;
+
     }
 
-    public Person(String name) {
+    public Person(String name) {  //this is your constructor
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
+
+     public Integer getWeight() {
+        return weight;}
+
+    public void setWeight(int weight) {this.weight = weight; }
+
+    public Integer getHeight() {return height;}
+
+    public void setHeight(int height) { this.height = height;}
+
+    public String getEyeColor() { return eyecolor;}
+
+    public void setEyeColor(String eyecolor) {this.eyecolor = eyecolor;}
+
+    public String getRace(){return race;}
+
+    public void setRace(String race){this.race = race;}
+
+    public String getGender(){return gender;}
+
+    public void setGender(String gender){this.gender = gender;}
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -14,7 +14,8 @@ public class TestPerson {
         Integer expectedAge = Integer.MAX_VALUE;
 
         // When
-        Person person = new Person();
+        Person person = new Person(); // new means that a new object was created
+        // this is calling a new object to the person constructor
 
         // Then
         String actualName = person.getName();
@@ -95,4 +96,132 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void getWeightTest(){
+        Person person = new Person();
+        Integer expected = 198;
+
+        //when
+        person.setWeight(expected);
+        Integer actual = person.getWeight();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setWeightTest(){
+        Person person = new Person();
+        Integer expected = 198;
+
+        //when
+        person.setWeight(expected);
+        Integer actual = person.getWeight();
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void getHeightTest(){
+        Person person = new Person();
+        Integer expected = 67;
+
+        //when
+        person.setHeight(expected);
+        Integer actual = person.getHeight();
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setHeightTest(){
+        Person person = new Person();
+        Integer expected = 67;
+
+        //when
+        person.setHeight(expected);
+        Integer actual = person.getHeight();
+
+        //then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void getEyeColorTest(){
+        Person person = new Person();
+        String expected = "hazel";
+
+        person.setEyeColor(expected);
+        String actual = person.getEyeColor();
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setEyeColorTest(){
+        Person person = new Person();
+        String expected = "hazel";
+
+        //when
+        person.setEyeColor(expected);
+        String actual = person.getEyeColor();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getRace(){
+        Person person = new Person();
+        String expected = "hazel";
+
+        //when
+        person.setRace(expected);
+        String actual = person.getRace();
+
+        //then
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+
+    public void setRace(){
+        Person person = new Person();
+        String expected = "Black";
+
+        //when
+        person.setRace(expected);
+        String actual = person.getRace();
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getGender(){
+        Person person = new Person();
+        String expected = "Woman";
+
+        // when
+        person.setGender(expected);
+        String actual = person.getGender();
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+
+    public void setGender(){
+        Person person = new Person();
+        String expected = "Woman";
+
+        //when
+        person.setGender(expected);
+        String actual = person.getGender();
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
